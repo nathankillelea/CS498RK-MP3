4cfc3507730913d3cc1961d5e0d2290f8957c474
bombolord <nathan.killelea@gmail.com>
2017-11-06 15:36:38 -0600

    finalfinalfinalrev

--- routes/users.js
+++ routes/users.js
@@ -35,10 +35,7 @@ module.exports = function (router) {
 		var newUser = new User();
 		newUser.name = req.body.name;
 		newUser.email = req.body.email;
-		if(req.body.pendingTasks == "undefined")
-			newUser.pendingTasks = [];
-		else
-			newUser.pendingTasks = req.body.pendingTasks;
+		newUser.pendingTasks = [];
 		User.findOne({email: newUser.email}).exec() // query for duplicate email
 		.then((user) => {
 			if(user == null) { // if there is no user with this email, save
